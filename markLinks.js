583cc7427db7798b912831b9d55de9ffaf2a24d7
Mark Jurkiewicz <markajurkiewicz@gmail.com>
2016-09-09 00:09:43 -0700

    experiment with domcontentloaded

--- markLinks.js
+++ markLinks.js
@@ -1,3 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    timeStamp();
+    viewTime();
+    handlers();
+
+});
+
 var timeStamp = {
 
     getQueryVariable : function () {
